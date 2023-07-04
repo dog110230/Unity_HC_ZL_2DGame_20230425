92a6e9358b936b45bed92bc6d7cae5d787580ca7
dog110230 <dog110230@gmail.com>
2023-07-04 21:33:56 +0800

    新增等級經驗

--- Assets/Scripts/DamageEnemy.cs
+++ Assets/Scripts/DamageEnemy.cs
@@ -2,12 +2,33 @@
 
 public class DamageEnemy : DamageBasic
 {
+    private DataEnemy dataEnemy;
+
+    private void Start()
+    {
+        dataEnemy = (DataEnemy) data;
+        print(dataEnemy.expProbability);
+    }
+    
     private void OnCollisionEnter2D(Collision2D collision)
     {
         if (collision.gameObject.name.Contains("武器"))
         {
             Damage(50);
             
+        }
+    }
+
+    protected override void Dead()
+    {
+        base.Dead();
+        Destroy(gameObject);
+
+        print("隨機值：" + Random.value);
+
+        if (Random.value < dataEnemy.expProbability)
+        {
+            Instantiate(dataEnemy.prefabExp, transform.position, transform.rotation);
         }
     }
 }
---
+++ Assets/Scripts/ExpSystem.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+public class ExpSystem : MonoBehaviour
+{
+    [Header("移動速度"), Range(0, 100)]
+    public float speed = 3.5f;
+
+
+    private Transform player;
+
+    private void Awake()
+    {
+        player = GameObject.Find("爆走企鵝").transform;
+    }
+
+    private void Update()
+    {
+        transform.position = Vector3.MoveTowards(transform.position, player.position, speed * Time.deltaTime);
+    }
+}
